6d355259f2589e8017fdde78c587d78bb46448dc
Jonathan Thorne <jonathanthorne13@gmail.com>
2022-03-08 17:53:55 -0500

    added some comments

--- utilities/arithmetic.py
+++ utilities/arithmetic.py
@@ -1,4 +1,4 @@
 from math import ceil
 
 def title_start(title, width):
-    return int((width // 2) - (len(title) // 2) - len(title) % 2)
+    return int((width // 2) - (len(title) // 2) - len(title) % 2) #I forgot how I came to this equation, but it perfectly centers the title. 
