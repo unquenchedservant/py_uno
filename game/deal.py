f24334f65c54c1bc43569a961e406648369bbc0e
Jonathan Thorne <jonathanthorne13@gmail.com>
2022-03-11 16:25:44 -0500

    Added python code to deal

--- game/deal.py
+++ game/deal.py
@@ -1,50 +1,39 @@
-class Deal:
-    #java adds private ArrayList<Card> hand;
-    #i believe the point of this it to establish a hand for the dealer. 
-    #If I remember correctly, this is a poorly titled class name that I may change later.
+class Deal: # why is this deal and not hand?
+
     def __init__(self):
-        pass
-        # hand = new Arraylist<>();
+        self.hand = []
 
-    def addCard(deck):
-        #these are overloaded
-        #this one in Java
-        #
-        # hand.add(deck.getLast())
-        # deck.removeLast()
-        
-        # This is for when a player needs to draw a new card
-    
-        pass
-    def addCard(addCard):
-        pass
-        #see above. 
-        #JAVA is hand.add(addCard)
-        #
-        #I suppose this is when you want to add a specific card. 
+    def addCardFromDeck(self, deck):
+        self.hand.append(deck.getLastCard())
+        deck.removeLastCard()
 
-    def removeCard(elem):
-        pass
-        #when you need to a remove a card from a player's hand, typically in playing the card. 
-        #JAVA hand.remove(elem) (remove is provided by the ArrayList library)    
-    def getCard(elem):
-        pass
-        #returns hand.get(elem)
-        #This is when you just want to get the card without doing anything to it
+    def addCard(self, addCard):
+        self.hand.append(addCard)
+
+    def removeCard(self,elem):
+        self.hand.remove(elem)
     
-    def getSize():
+    def getCard(self, elem):
+        return self.hand.get(elem)
+
+    ''' def getSize():
         pass
         #returns hand.size(); (size is provided by the ArrayList library)
-    
-    def printArray():
+    '''
+
+    '''def printArray():
         pass
         #I recall hating this method name as well
         #Java : System.out.println(hand.toString()); (hand.toString() is provided by the 
         # card object)
-
+    '''
+    '''
     def getLast():
         pass
-        #return hand.get(hand.size()-1)
+        #return hand.get(hand.size()-1) 
         #not really sure why this couldn't just be utilized with the getCard function. 
 
+    
+
+    '''
     
